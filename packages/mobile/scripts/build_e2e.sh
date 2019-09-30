b025479ddbf35a3b9edd7096efb3bb998daa7edd
Martín Volpe <martin.volpe@gmail.com>
2019-09-30 16:38:21 +0200

    Don't build the app to save time

--- packages/mobile/scripts/build_e2e.sh
+++ packages/mobile/scripts/build_e2e.sh
@@ -9,7 +9,7 @@ ENVFILE=".env.test"
 # set the IS_E2E_ENV value in .env during build only if it does not exist already
 grep -qF -- "IS_E2E=1" $ENVFILE || printf "\nIS_E2E=1" >> $ENVFILE
 
-ENVFILE=$ENVFILE yarn detox build -c android.emu.debug
+# ENVFILE=$ENVFILE yarn detox build -c android.emu.debug
 
 # Remove the temp flag from file
 # sed -i '' '/IS_E2E=1/d' $ENVFILE
